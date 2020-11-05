6dcff5bbfd3c3bc54aba891a8f2f957450927cb6
Christian G. Warden <cwarden@xerus.org>
2020-11-05 12:52:14 -0800

    Add completion Command
    Add completion command to generate shell completion code.

---
+++ cmd/completion.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
+
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate completion script",
+	Long: `To load completions:
+
+Bash:
+
+$ source <(force-md completion bash)
+
+# To load completions for each session, execute once:
+Linux:
+  $ force-md completion bash > /etc/bash_completion.d/force-md
+MacOS:
+  $ force-md completion bash > /usr/local/etc/bash_completion.d/force-md
+
+Zsh:
+
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+# To load completions for each session, execute once:
+$ force-md completion zsh > "${fpath[1]}/_force-md"
+
+# You will need to start a new shell for this setup to take effect.
+`,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletion(os.Stdout)
+		}
+	},
+}
