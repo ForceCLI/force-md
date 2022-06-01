da1c373441160813a3d18d9ea8ff57887fdb2179
Christian G. Warden <cwarden@xerus.org>
2022-06-01 14:39:35 -0500

    Add Negated Filter Options to `objects fields list`

--- general/types.go
+++ general/types.go
@@ -23,3 +23,11 @@ type IntegerText struct {
 func (b *BooleanText) ToBool() bool {
 	return b != nil && strings.ToLower(b.Text) == "true"
 }
+
+func (b *BooleanText) IsTrue() bool {
+	return b != nil && strings.ToLower(b.Text) == "true"
+}
+
+func (b *BooleanText) IsFalse() bool {
+	return b != nil && strings.ToLower(b.Text) == "false"
+}
