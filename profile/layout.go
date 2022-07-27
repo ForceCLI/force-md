bb81f56dcbe1754cbfaaa1fb82927e5be2bc61a0
Christian G. Warden <cwarden@xerus.org>
2022-07-27 08:13:53 -0500

    Add --layout Flag for `profile layout delete`
    Add option to delete a specific layout from profiles.

--- profile/layout.go
+++ profile/layout.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+type LayoutFilter func(LayoutAssignment) bool
+
 func (p *Profile) GetLayouts(objectName string) LayoutAssignmentList {
 	layoutPrefix := objectName + "-"
 	var layouts LayoutAssignmentList
@@ -52,16 +54,23 @@ func (p *Profile) SetObjectLayoutForRecordType(objectName, layoutName, recordTyp
 	p.LayoutAssignments.Tidy()
 }
 
-func (p *Profile) DeleteObjectLayout(objectName string) error {
+func (p *Profile) DeleteObjectLayout(objectName string, filters ...LayoutFilter) error {
 	found := false
 	newLayouts := p.LayoutAssignments[:0]
 	layoutPrefix := strings.ToLower(objectName + "-")
+LAYOUTS:
 	for _, l := range p.LayoutAssignments {
-		if strings.HasPrefix(strings.ToLower(l.Layout), layoutPrefix) {
-			found = true
-		} else {
+		if !strings.HasPrefix(strings.ToLower(l.Layout), layoutPrefix) {
 			newLayouts = append(newLayouts, l)
+			continue
 		}
+		for _, filter := range filters {
+			if !filter(l) {
+				newLayouts = append(newLayouts, l)
+				continue LAYOUTS
+			}
+		}
+		found = true
 	}
 	if !found {
 		return errors.New("layout assignment not found")
