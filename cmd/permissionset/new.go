97da3b90c8c1823f3d9cd7dacff8c7843f50c41a
Christian G. Warden <cwarden@xerus.org>
2022-08-24 11:59:09 -0500

    Add `permissionset edit` Command

--- cmd/permissionset/new.go
+++ cmd/permissionset/new.go
@@ -12,12 +12,15 @@ import (
 var (
 	label       string
 	description string
+	license     string
 )
 
 func init() {
 	NewCmd.Flags().StringVarP(&label, "label", "l", "", "label")
 	NewCmd.Flags().StringVarP(&description, "description", "d", "", "description")
 	NewCmd.MarkFlagRequired("label")
+
+	EditCmd.Flags().StringVarP(&license, "license", "i", "", "license")
 }
 
 var NewCmd = &cobra.Command{
@@ -27,6 +30,17 @@ var NewCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		for _, file := range args {
 			addPermissionSet(file)
+		}
+	},
+}
+
+var EditCmd = &cobra.Command{
+	Use:   "edit [flags] [filename]...",
+	Short: "Edit permission set",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, file := range args {
+			updatePermissionSet(file)
 		}
 	},
 }
@@ -45,6 +59,24 @@ func addPermissionSet(file string) {
 	err := internal.WriteToFile(p, file)
 	if err != nil {
 		log.Warn("write failed: " + err.Error())
+		return
+	}
+}
+
+func updatePermissionSet(file string) {
+	p, err := permissionset.Open(file)
+	if err != nil {
+		log.Warn("parsing permission set failed: " + err.Error())
+		return
+	}
+	if license != "" {
+		p.License = &permissionset.License{
+			Text: license,
+		}
+	}
+	err = internal.WriteToFile(p, file)
+	if err != nil {
+		log.Warn("update failed: " + err.Error())
 		return
 	}
 }
